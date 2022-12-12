0cdf87661d13222721914bf7a05ac7f5e42e23db
Obay <ahmad.obay@gmail.com>
2022-12-12 09:47:26 -0800

    Randomize storage account name

--- runme.sh
+++ runme.sh
@@ -4,9 +4,13 @@ export TF_VAR_mypublicipaddress=$(curl -s ifconfig.me/ip)
 export TF_VAR_adminpassword=$(pwgen 12 1)
 export TF_VAR_tenant_id=$(az account show | jq -r ".tenantId")
 export TF_VAR_subscription_id=$(az account show | jq -r ".id")
-export TF_VAR_storusaz140name="storusaz140jag"
-export TF_VAR_storjwaz140name="storjwaz140jag"
-export TF_VAR_storukaz140name="storukaz140jag"
+RANDOM_STORAGE_ACCOUNT_SUFFIX=$(pwgen 4 1)
+if test -f "RANDOM_STORAGE_ACCOUNT_SUFFIX"; then
+    RANDOM_STORAGE_ACCOUNT_SUFFIX=$(cat RANDOM_STORAGE_ACCOUNT_SUFFIX)
+fi
+export TF_VAR_storusaz140name="storusaz140$RANDOM_STORAGE_ACCOUNT_SUFFIX"
+export TF_VAR_storjwaz140name="storjwaz140$RANDOM_STORAGE_ACCOUNT_SUFFIX"
+export TF_VAR_storuksaz140name="storukaz140$RANDOM_STORAGE_ACCOUNT_SUFFIX"
 
 tfren
 terraform init
