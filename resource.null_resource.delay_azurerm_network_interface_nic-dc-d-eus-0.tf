a1051c48c183d8e93d9e4ce9cc8d77e65c1d3b02
Obay <ahmad.obay@gmail.com>
2023-01-05 14:19:58 -0800

    Delete redundant null resources.

--- resource.null_resource.delay_azurerm_network_interface_nic-dc-d-eus-0.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "delay_azurerm_network_interface_nic-dc-d-eus-0" {
-  provisioner "local-exec" {
-    command = "sleep 30"
-  }
-
-  triggers = {
-    "before" = "${azurerm_network_interface.nic-dc-d-eus-0.id}"
-  }
-}
--- resource.null_resource.delay_azurerm_network_interface_nic-dc-d-eus-1.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "delay_azurerm_network_interface_nic-dc-d-eus-1" {
-  provisioner "local-exec" {
-    command = "sleep 30"
-  }
-
-  triggers = {
-    "before" = "${azurerm_network_interface.nic-dc-d-eus-1.id}"
-  }
-}
