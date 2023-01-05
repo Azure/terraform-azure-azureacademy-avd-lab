4ce216b0090091f05c3d480383e1543e9904aaa7
Obay <ahmad.obay@gmail.com>
2023-01-05 14:19:07 -0800

    Update runme.sh to be more generic

--- runme.sh
+++ runme.sh
@@ -2,18 +2,19 @@
 
 export TF_VAR_mypublicipaddress=$(curl -s ifconfig.me/ip)
 export TF_VAR_adminpassword=$(pwgen 12 1)
-if test -f "adminpassword"; then
+if test -f "adminpassword"
+then
     TF_VAR_adminpassword=$(cat adminpassword)
 else
-    echo $TF_VAR_adminpassword > adminpassword
+    echo -n $TF_VAR_adminpassword > adminpassword
 fi
 export TF_VAR_tenant_id=$(az account show | jq -r ".tenantId")
 export TF_VAR_subscription_id=$(az account show | jq -r ".id")
-RANDOM_STORAGE_ACCOUNT_SUFFIX=$(pwgen 4 1)
+RANDOM_STORAGE_ACCOUNT_SUFFIX=$(pwgen 4 1 | tr '[:upper:]' '[:lower:]')
 if test -f "RANDOM_STORAGE_ACCOUNT_SUFFIX"; then
     RANDOM_STORAGE_ACCOUNT_SUFFIX=$(cat RANDOM_STORAGE_ACCOUNT_SUFFIX)
 else
-    echo $RANDOM_STORAGE_ACCOUNT_SUFFIX > RANDOM_STORAGE_ACCOUNT_SUFFIX
+    echo -n $RANDOM_STORAGE_ACCOUNT_SUFFIX > RANDOM_STORAGE_ACCOUNT_SUFFIX
 fi
 export TF_VAR_storusaz140name="storusaz140$RANDOM_STORAGE_ACCOUNT_SUFFIX"
 export TF_VAR_storjwaz140name="storjwaz140$RANDOM_STORAGE_ACCOUNT_SUFFIX"
@@ -24,5 +25,5 @@ terraform init
 terraform fmt
 
 # DANGUROUS: THIS IF FOR DEMO PURPOSES ONLY
-terraform apply --auto-approve
+time terraform apply --auto-approve
 # terraform destroy --auto-approve
