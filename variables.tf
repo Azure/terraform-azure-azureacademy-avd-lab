911bbcc61314b588672e38025548f4484ecf3369
Obay <ahmad.obay@gmail.com>
2023-02-07 22:03:44 -0800

    Add Azure environment variable declaration to silance Terraform Cloud warnings

--- variables.tf
+++ variables.tf
@@ -53,7 +53,14 @@ variable "ARM_TENANT_ID" {
   type = string
 }
 
-variable "ARM_CLIENT_SECRET" {
+variable "ARM_SUBSCRIPTION_ID" {
   type = string
 }
 
+variable "ARM_CLIENT_ID" {
+  type = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type = string
+}
